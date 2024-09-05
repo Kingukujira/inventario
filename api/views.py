7b87b76acb93b69a90cca532e6d293c8d355fb43
Johan <johanmag318@gmail.com>
2024-09-04 19:07:30 -0500

    Correccion

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from .serializer import userSerializer
+from .serializer import userSerializer, comentarioSerializer, departamentoSerializer, municipioSerializer, sitioTuristicoSerializer, tipoServicioSerializer, servicioSerializer
 from .models import Usuario, Comentario, Departamento, Municipio, SitioTuristico, TipoServicio, Servicio
 
 # Create your views here.
@@ -10,24 +10,24 @@ class userViewSet(viewsets.ModelViewSet):
 
 class comentarioViewSet(viewsets.ModelViewSet):
     queryset = Comentario.objects.all()
-    serializer_class = userSerializer
+    serializer_class = comentarioSerializer
 
 class departamentoViewSet(viewsets.ModelViewSet):
     queryset = Departamento.objects.all()
-    serializer_class = userSerializer
+    serializer_class = departamentoSerializer
 
 class municipioViewSet(viewsets.ModelViewSet):
     queryset = Municipio.objects.all()
-    serializer_class = userSerializer
+    serializer_class = municipioSerializer
 
 class sitioTuristicoViewSet(viewsets.ModelViewSet):
     queryset = SitioTuristico.objects.all()
-    serializer_class = userSerializer
+    serializer_class = sitioTuristicoSerializer
 
 class tipoServicioViewSet(viewsets.ModelViewSet):
     queryset = TipoServicio.objects.all()
-    serializer_class = userSerializer
+    serializer_class = tipoServicioSerializer
 
 class servicioViewSet(viewsets.ModelViewSet):
     queryset = Servicio.objects.all()
-    serializer_class = userSerializer
+    serializer_class = servicioSerializer
